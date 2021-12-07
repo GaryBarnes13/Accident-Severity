40cdebdf8cd2a2ca6c88f48025a615096392ea1c
asayadi <59611091+asayadi@users.noreply.github.com>
2021-12-06 23:01:11 -0500

    splitting is outputting correctly in splitting.py

--- splitting.py
+++ splitting.py
@@ -1,4 +1,52 @@
-from typing import TYPE_CHECKING, type_check_only
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+df = pd.read_csv("/Users/andrewsayadi/Downloads/US_Accidents_Dec20_updated.csv")
+X = df.head(1000)
+X = df.drop(['ID',
+               'Severity',
+               'Start_Lat',
+               'End_Lat',
+               'Start_Lng',
+               'End_Lng',
+               'Distance(mi)',
+               'Start_Time',
+               'End_Time',
+               'Description'
+               ], axis=1)
+y = df["Severity"].head(1000)
+
+class MultiColumnLabelEncoder:
+    def __init__(self,columns = None):
+        self.columns = columns # array of column names to encode
+
+    def fit(self,X,y=None):
+        return self # not relevant here
+
+    def transform(self,X):
+        '''
+        Transforms columns of X specified in self.columns using
+        LabelEncoder(). If no columns specified, transforms all
+        columns in X.
+        '''
+        output = X.copy()
+        if self.columns is not None:
+            for col in self.columns:
+                output[col] = LabelEncoder().fit_transform(output[col])
+        else:
+            for colname,col in output.iteritems():
+                output[colname] = LabelEncoder().fit_transform(col)
+        return output
+
+    def fit_transform(self,X,y=None):
+        return self.fit(X,y).transform(X)
+
+X_labeled = MultiColumnLabelEncoder(columns = ['Street','Side','City','County','State','Zipcode','Country','Timezone','Airport_Code','Weather_Timestamp','Wind_Direction','Weather_Condition','Amenity','Bump','Crossing','Give_Way','Junction','No_Exit','Railway','Roundabout','Station','Stop','Traffic_Calming','Traffic_Signal','Turning_Loop','Turning_Loop','Sunrise_Sunset','Civil_Twilight','Nautical_Twilight','Astronomical_Twilight']).fit_transform(X)
+
+X_labeled = X_labeled.fillna(0)
+X_labeled = X_labeled.head(1000)
+
+#from typing import TYPE_CHECKING, type_check_only
 from numpy import testing
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -6,25 +54,55 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import KFold
 from sklearn.model_selection import StratifiedKFold
+from sklearn import tree
+
 
 def split():
-        df = pd.read_csv("/Users/andrewsayadi/Library/Mobile Documents/com~apple~CloudDocs/Data Mining/Accident-Severity/Accident-Severity/US_Accidents_Dec20_updated.csv")
-        
-        X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.3) #need to know the data and target we are using
-        
-        lr = LogisticRegression()
-        lr.fit(X_train, y_train)
-        lr.score(X_test, y_test) #wont run yet cause we havent define data and target
 
-        rf = RandomForestClassifier(n_estimators = 40)
-        rf.fit(X_train, y_train)
-        rf.score(X_test, y_test) #need to define data and target
+        # X_train, X_test, y_train, y_test = train_test_split(X_labeled, y, test_size=0.3) #need to know the data and target we are using
+        
+        # lr = tree.DecisionTreeClassifier()
+        # lr.fit(X_train, y_train)
+        # print('train test split decision tree: ', lr.score(X_test, y_test))
+    
 
-        kf = KFold(n_splits = 10)
-        for train_index, test_index in kf.split(data): #data is not defined yet
-            pass
+        # rf = RandomForestClassifier(n_estimators = 40)
+        # rf.fit(X_train, y_train)
+        # print('train test split Random Forest: ', rf.score(X_test, y_test))
+
+
+
+
+        # kf = KFold(n_splits = 10)                                     
+        # for train_index, test_index in kf.split(X_labeled):
+        #     #print("TRAIN:", train_index, "TEST:", test_index)
+        #     X_train_KFold, X_test_KFold = X_labeled.loc[train_index], X_labeled.loc[test_index]
+        #     y_train_KFold, y_test_KFold = y.loc[train_index], y.loc[test_index]
+
+        # lr_KFold = tree.DecisionTreeClassifier()
+        # lr_KFold.fit(X_train_KFold, y_train_KFold)
+        # print('KFold decision tree: ', lr_KFold.score(X_test_KFold, y_test_KFold))
+
+        # rf_KFold = RandomForestClassifier(n_estimators = 40)
+        # rf_KFold.fit(X_train_KFold, y_train_KFold)
+        # print('KFold Random Forest: ', rf_KFold.score(X_test_KFold, y_test_KFold))
+
+
 
         folds = StratifiedKFold(n_splits = 10)
+        for train_index, test_index in folds.split(X_labeled, y):
+             #print("TRAIN:", train_index, "TEST:", test_index)
+             X_train_stratified, X_test_stratified = X_labeled.loc[train_index], X_labeled.loc[test_index]
+             y_train_stratified, y_test_stratified = y.loc[train_index], y.loc[test_index]
+
+        # lr_stratified = tree.DecisionTreeClassifier()
+        # lr_stratified.fit(X_train_stratified, y_train_stratified)
+        # print('Stratified KFold decision tree: ', lr_stratified.score(X_test_stratified, y_test_stratified))
+
+        rf_stratified = RandomForestClassifier(n_estimators = 40)
+        rf_stratified.fit(X_train_stratified, y_train_stratified)
+        print('Stratified KFold Random Forest: ', rf_stratified.score(X_test_stratified, y_test_stratified))
+
         
 split()
 
